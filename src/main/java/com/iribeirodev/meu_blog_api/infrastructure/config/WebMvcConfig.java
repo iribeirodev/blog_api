704152ce08a789ee8ac2debb974d81bd4d5cc269
Itamar Ribeiro <ribeirojr.itamar@gmail.com>
2024-10-25 17:39:57 -0300

    testando configurações de ambiente

--- src/main/java/com/iribeirodev/meu_blog_api/infrastructure/config/WebMvcConfig.java
+++ src/main/java/com/iribeirodev/meu_blog_api/infrastructure/config/WebMvcConfig.java
@@ -1,8 +1,10 @@
 package com.iribeirodev.meu_blog_api.infrastructure.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.method.HandlerTypePredicate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,8 +13,19 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  */
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer{
+    @Value("${CORS_ALLOWED_ORIGINS}")
+    private String corsAllowedOrigins;
+
 	@Override
 	public void configurePathMatch(PathMatchConfigurer configurer) {
 		configurer.addPathPrefix("api", HandlerTypePredicate.forAnnotation(RestController.class));
 	}
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins(corsAllowedOrigins)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
+    }
 }
