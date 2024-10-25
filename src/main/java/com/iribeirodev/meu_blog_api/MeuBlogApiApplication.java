7414eba73a3680b43b04f12c79aacb85efc18493
Itamar Ribeiro <ribeirojr.itamar@gmail.com>
2024-10-25 16:20:40 -0300

    Alterações para corrigir leitura das variaveis de ambiente

--- src/main/java/com/iribeirodev/meu_blog_api/MeuBlogApiApplication.java
+++ src/main/java/com/iribeirodev/meu_blog_api/MeuBlogApiApplication.java
@@ -3,16 +3,11 @@ package com.iribeirodev.meu_blog_api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class MeuBlogApiApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
-        System.setProperty("spring.datasource.url", dotenv.get("DB_URL"));
-        System.setProperty("spring.datasource.username", dotenv.get("DB_USERNAME"));
-        System.setProperty("spring.datasource.password", dotenv.get("DB_PASSWORD"));
 		SpringApplication.run(MeuBlogApiApplication.class, args);
 	}
 }
